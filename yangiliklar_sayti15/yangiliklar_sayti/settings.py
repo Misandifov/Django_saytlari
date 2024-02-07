117300864cdf3a86dc7f2ed891d8106d43260678
Misandifov <137529811+Misandifov@users.noreply.github.com>
2024-02-07 22:44:55 +0500

    yangilandi

--- yangiliklar_sayti15/yangiliklar_sayti/settings.py
+++ yangiliklar_sayti15/yangiliklar_sayti/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-brgo0qxm=a%a^ir92lc8x-+m2meww)o(zk+@830-!!u1n2amc)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- yangiliklar_sayti15/yangiliklar_sayti/urls.py
+++ yangiliklar_sayti15/yangiliklar_sayti/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('reset-password', PasswordResetView.as_view(), name='reset-password')
 ]
 if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,
-                        document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
